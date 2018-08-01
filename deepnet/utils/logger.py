6e0243f702a79c7ac9d2f08c5c8f4bc578c0d5e6
elda27 <kaz.birdstick@gmail.com>
2018-08-01 10:28:18 +0900

    Modify log feature To log w/ and w/o weight

--- deepnet/utils/logger.py
+++ deepnet/utils/logger.py
@@ -3,42 +3,49 @@ import os.path
 from contextlib import ExitStack
 import chainer
 from itertools import zip_longest
+from collections import OrderedDict
 
 class Logger:
     def __init__(self, output_filename, dump_variables, variable_weights):
         self.output_filename = output_filename
         self.dump_variables = dump_variables
+        self.weights = variable_weights
         
         if os.path.exists(output_filename):
             os.remove(output_filename)
 
     def __call__(self, variables, is_valid=False):
-        dump_vars = {}
+        dump_vars = OrderedDict()
+        added_vars = OrderedDict()
+
         for var_name, weight in zip_longest(self.dump_variables, self.weights):
             if var_name not in variables:
-                data[var_name] = ''
+                dump_vars[var_name] = ''
+                if weight is not None and not (isinstance(weight, str) and weight == ''):
+                    added_vars[var_name + '.raw'] = ''
+                continue
             
-            if weight is not None or (isinstance(weight, str) and weight != ''):
-                data[var_name] = variables[var_name] * float(weight)
-                data[var_name + '.raw'] = variables[var_name]
+            if weight is not None and not (isinstance(weight, str) and weight == ''):
+                dump_vars[var_name] = variables[var_name] * float(weight)
+                added_vars[var_name + '.raw'] = variables[var_name]
             else:
-                data[var_name] = variables[var_name]
-
-        dump_vars = { var_name:variables[var_name] if var_name in variables else '' for var_name in self.dump_variables }
-        self.dump(**dump_vars)
+                dump_vars[var_name] = variables[var_name]
+        
+        dump_vars.update(added_vars)
+        self.dump(dump_vars)
 
     @abstractmethod
-    def dump(self, **kwargs):
+    def dump(self, dump_vars):
         raise NotImplementedError()
 
 class CsvLogger(Logger):
-    def dump(self, **kwargs):
+    def dump(self, dump_vars):
         with ExitStack() as estack:
             fp = None
             if not os.path.exists(self.output_filename):
                 fp = estack.enter_context(open(self.output_filename, 'w+'))
-                fp.write(','.join(self.dump_variables) + '\n')
+                fp.write(','.join(dump_vars) + '\n')
             else:
                 fp = estack.enter_context(open(self.output_filename, 'a'))
 
-            fp.write(','.join([str(kwargs[var_name]) for var_name in self.dump_variables]) + '\n')
+            fp.write(','.join([ str(dump_vars[var_name]) for var_name in dump_vars ]) + '\n')
