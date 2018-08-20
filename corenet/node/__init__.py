5144a7cbead7bcb607e2da9fbc36997dac97044c
elda27 <kaz.birdstick@gmail.com>
2018-08-21 08:16:31 +0900

    Modify a implementation of make_node

--- corenet/node/__init__.py
+++ corenet/node/__init__.py
@@ -1,19 +1,48 @@
 from enum import Enum
+from functools import wraps
+
 from .network_node import NetworkNode
 from .updatable_node import UpdatableNode
+from .chainer_node   import ChainerNode
+from .interable_node import IterableNode
 
 class NodeType(Enum):
     Default   = 'default'
     Updatable = 'updatable'
+    Chainer   = 'chainer'
+    Iterable  = 'iterable'
 
+_map_processor_type = {
+    NodeType.Default   : NetworkNode,
+    NodeType.Updatable : UpdatableNode,
+    NodeType.Chainer   : ChainerNode,
+    NodeType.Iterable  : IterableNode,
+}
 _declared_processors = {}
 
 def declare_node_type(node_type, **kwargs):
+    """Declare node type for make_node function.
+    
+    Args:
+        node_type (str): Node type to generate node wrapping any processor.
+    """
+
     def _declare_node_type(klass):
-        if node_type == NodeType.Updatable:
-            _declare_node_type[klass] = (UpdatableNode, kwargs)
+        assert node_type in _map_processor_type, 'Unknown node type:' + node_type
+        _declared_processors[klass] = (_map_processor_type[node_type], kwargs)
+        return klass
+    return declare_node_type
 
 def make_node(model, **kwargs):
+    """Generator function of network node.
+    
+    Args:
+        model (function): Process function
+    
+    Returns:
+        node_type: Generated node
+    """
+
     klass = type(model)
     node_type = NetworkNode
     if klass in _declared_processors:
