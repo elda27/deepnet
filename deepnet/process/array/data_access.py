b0d4cb2f4de5ac1893fb518726d7d1e68162e1f3
elda27 <kaz.birdstick@gmail.com>
2018-09-20 10:44:35 +0900

    Add iterable processor

--- deepnet/process/array/data_access.py
+++ deepnet/process/array/data_access.py
@@ -1,7 +1,9 @@
+from corenet import declare_node_type
 from deepnet.core.registration import register_process
 from deepnet.core import config
 import chainer
 import chainer.functions as F
+import numpy as np
 import random
 
 
@@ -14,3 +16,18 @@ def get_element(*input, index=0, axis=1):
 def get_random_element(*input, axis=1):
     n = max([i.shape[axis] for i in input])
     return get_element(*input, index=random.randint(0, n), axis=axis)
+
+
+@declare_node_type('iterable')
+@register_process()
+def volume_to_slice(*input):
+    for i in zip(*input):
+        yield i
+
+
+@register_process()
+def merge_slice_to_volume(*input):
+    output = []
+    for i in input:
+        output.append(np.array(i))
+    return output
