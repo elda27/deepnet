4886b45d77c81a26bc529b95614e6e6b5276f35c
elda27 <kaz.birdstick@gmail.com>
2018-09-26 16:19:09 +0900

    Change generation method repr to str

--- corenet/node/network_node.py
+++ corenet/node/network_node.py
@@ -1,28 +1,31 @@
 from .param_holder import ParamHolder
 
+
 class NetworkNode:
     DeclareArguments = {}
 
     def __init__(
         self, name, model,
         input, output, args,
-        using_internal_param = [],
+        using_internal_param=[],
         **kwargs
     ):
         input_ = input
-        self.name  = name
+        self.name = name
 
-        self.input = input_ if isinstance(input_, list) else [ input_ ] 
-        self.output = output if isinstance(output, list) else [ output ] 
+        self.input = input_ if isinstance(input_, list) else [input_]
+        self.output = output if isinstance(output, list) else [output]
 
         def recusrsive_check_instance(a):
             if isinstance(a, (list, tuple)):
-                return all([ recusrsive_check_instance(e) for e in a ])
+                return all([recusrsive_check_instance(e) for e in a])
             else:
                 return isinstance(a, str)
 
-        assert recusrsive_check_instance(self.input),  'Input must be string: {}'.format(self.input)
-        assert recusrsive_check_instance(self.output), 'Output must be string: {}'.format(self.output)
+        assert recusrsive_check_instance(
+            self.input),  'Input must be string: {}'.format(self.input)
+        assert recusrsive_check_instance(
+            self.output), 'Output must be string: {}'.format(self.output)
 
         self.model = model
         self.args = args
@@ -31,11 +34,11 @@ class NetworkNode:
         if issubclass(type(self.model), ParamHolder):
             for u in using_internal_param:
                 ParamHolder.request_param(self.model, u)
-    
+
     def __call__(self, *args):
         return self.model(*args, **self.args)
 
-    def __repr__(self):
+    def __str__(self):
         return str(dict(input=self.input, output=self.output))
 
     def get_internal_param(self, fields):
@@ -48,7 +51,8 @@ class NetworkNode:
 
         if issubclass(type(self.model), ParamHolder):
             if not hasattr(proc, 'stores'):
-                raise NotImplementedError('get_internal_param method is not fully implementation')
+                raise NotImplementedError(
+                    'get_internal_param method is not fully implementation')
             return proc.stores[fields[-1]]
         else:
             return ParamHolder.get_param(proc, fields[-1])
