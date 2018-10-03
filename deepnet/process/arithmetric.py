8e412cf04be1977b51113fc40eff0d43afa689f5
elda27 <kaz.birdstick@gmail.com>
2018-10-03 17:48:09 +0900

    For pep8

--- deepnet/process/arithmetric.py
+++ deepnet/process/arithmetric.py
@@ -7,10 +7,11 @@ operation_list = {
     '/': lambda x, y: x / y,
 }
 
+
 @register_process()
 def reduce(*input, operation='+', weights=None):
     if weights is None:
-        weights = [ 1.0 for _ in range(len(input)) ]
+        weights = [1.0 for _ in range(len(input))]
 
     operation = operation_list[operation]
     input_iter = iter(zip(input, weights))
