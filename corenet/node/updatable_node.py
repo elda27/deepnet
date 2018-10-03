555e7a153285aa7b80d0d3c24bd478f22da86837
elda27 <kaz.birdstick@gmail.com>
2018-10-03 17:47:30 +0900

    Fixed to call constructor of super class

--- corenet/node/updatable_node.py
+++ corenet/node/updatable_node.py
@@ -2,10 +2,12 @@ from .network_node import NetworkNode
 from contextlib import contextmanager
 from abc import abstractmethod
 
+
 class UpdatableNode(NetworkNode):
-    def __init__(self, update = None, **kwargs):
-        self.udpate_variable = update
-    
+    def __init__(self, update=None, **kwargs):
+        super().__init__(**kwargs)
+        self.update_variable = update
+
     def update(self, variable):
         yield self.update_before()
         yield self.update_core(variable[self.update_variable])
