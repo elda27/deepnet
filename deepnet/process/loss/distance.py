87b0778831bce2f1c898624585ea90a0e9853bd0
elda27 <kaz.birdstick@gmail.com>
2018-10-03 17:49:04 +0900

    Add normalization feature

--- deepnet/process/loss/distance.py
+++ deepnet/process/loss/distance.py
@@ -2,7 +2,11 @@ from deepnet.core.registration import register_process
 import chainer.functions as F
 import functools
 
+
 @register_process('loss.euclidean_distance')
-def euclidean_distance(x, t):
-    linear_shape = (x.shape[0], functools.reduce(lambda x,y: x * y, x.shape[1:]))
-    return F.mean(F.sqrt(F.batch_l2_norm_squared(F.reshape(x - t, linear_shape))))
+def euclidean_distance(x, t, normalize=False):
+    l2_norm = F.sqrt(F.batch_l2_norm_squared(x-t))
+    if normalize:
+        linear_shape = functools.reduce(lambda x, y: x * y, x.shape[1:])
+        l2_norm = l2_norm / linear_shape
+    return F.mean(l2_norm)
