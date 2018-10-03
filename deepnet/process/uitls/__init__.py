3ab91260e00ecfb426e08d5221c277d0851e9d7a
elda27 <kaz.birdstick@gmail.com>
2018-10-03 17:51:02 +0900

    Modify to support cast type for numpy.array

--- deepnet/process/uitls/__init__.py
+++ deepnet/process/uitls/__init__.py
@@ -1,6 +1,9 @@
 from deepnet.core.registration import register_process
 from deepnet.core import config
 import chainer
+import numpy as np
+import cupy as cp
+
 
 @register_process()
 def expand_dims(*input, axis=1):
@@ -10,13 +13,20 @@ def expand_dims(*input, axis=1):
         output.append(xp.expand_dims(i, axis=axis))
     return output
 
+
 @register_process()
 def cast_type(x, dtype):
-    return chainer.Variable(x.data.astype(dtype))
+    xp = chainer.cuda.get_array_module(x)
+    if isinstance(x, xp.ndarray):
+        return x.astype(dtype)
+    else:
+        return chainer.Variable(x.data.astype(dtype))
+
 
 @register_process()
 def bias(x, multiply=1.0, bias_=1.0):
     return x * multiply + bias_
+
 
 @register_process()
 def get_latent_representation(*_, source, network='main_network'):
