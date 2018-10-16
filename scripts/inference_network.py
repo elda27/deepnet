13efb7ae180d7718ff1ce7099ec5ce275a84a20c
elda27 <kaz.birdstick@gmail.com>
2018-10-16 11:05:12 +0900

    Fixed for runtime error

--- scripts/inference_network.py
+++ scripts/inference_network.py
@@ -1,6 +1,6 @@
 import auto_init
 import argparse
-from deepnet.utils.network import NetworkNode
+from corenet import NetworkNode
 from deepnet.utils import mhd
 import deepnet
 import toml
@@ -23,6 +23,7 @@ import json
 import log_util
 import tqdm
 
+
 def main():
     parser = build_arguments()
     args = parser.parse_args()
@@ -34,19 +35,21 @@ def main():
     args.batch_size = sum(args.batch_size)
 
     assert args.step_index > 0
-    deepnet.utils.config.set_global_config('gpu_id', args.gpu)
-    deepnet.utils.config.set_global_config('batch_size', args.batch_size)
+    deepnet.core.config.set_global_config('gpu_id', args.gpu)
+    deepnet.core.config.set_global_config('batch_size', args.batch_size)
 
     dataset_config = deepnet.config.load(args.dataset_config)
     test_index = deepnet.utils.parse_index_file(args.test_index)
 
-    test_dataset = deepnet.utils.dataset.GeneralDataset(dataset_config, test_index)
+    test_dataset = deepnet.utils.dataset.GeneralDataset(
+        dataset_config, test_index)
     if test_index is None:
         test_index = test_dataset.case_names
 
     log_dir = deepnet.utils.get_log_dir(args.log_root_dir, args.log_index)
 
-    visualize_dir = os.path.join(log_dir, 'visualize_step{}'.format(args.step_index))
+    visualize_dir = os.path.join(
+        log_dir, 'visualize_step{}'.format(args.step_index))
     archive_dir = os.path.join(log_dir, 'model_step{}'.format(args.step_index))
     param_dir = os.path.join(log_dir, 'param_step{}'.format(args.step_index))
     test_dir = os.path.join(log_dir, 'test_step{}'.format(args.step_index))
@@ -67,17 +70,21 @@ def main():
         with open(os.path.join(log_dirs['param'], 'network_config.json')) as fp:
             network_config = json.load(fp)
     else:
-        network_config = deepnet.config.load(args.network_config, is_variable_expansion=False)
-        network_config = update_log_dir(network_config, log_dirs) # Update log directory
+        network_config = deepnet.config.load(
+            args.network_config, is_variable_expansion=False)
+        network_config = update_log_dir(
+            network_config, log_dirs)  # Update log directory
         network_config = deepnet.config.expand_variable(network_config)
-    network_manager, visualizers = deepnet.core.registration .build_networks(network_config)
+    network_manager, visualizers = deepnet.core.build_networks(network_config)
 
-    for name, proc in deepnet.core.registration ._created_process.items():
-        if proc in deepnet.core.registration ._updatable_process:
+    for name, proc in deepnet.core.get_created_process_list().items():
+        if proc in deepnet.core.get_updatable_process_list():
             continue
-        model_list = list(glob.glob(os.path.join(archive_dir, name + '_*.npz')))
+        model_list = list(
+            glob.glob(os.path.join(archive_dir, name + '_*.npz')))
         if len(model_list) == 0:
-            raise ValueError('Model not found: {} in {}'.format(name, archive_dir))
+            raise ValueError(
+                'Model not found: {} in {}'.format(name, archive_dir))
         model_filename = model_list[-1]
         chainer.serializers.load_npz(model_filename, proc['proc'])
         proc['proc'].to_gpu()
@@ -94,8 +101,9 @@ def main():
     # Start inference.
     variables = {}
     encoded_codes_list = []
-    index_list = { idx: 0 for idx in test_index }
-    test_iterator = chainer.iterators.MultiprocessIterator(test_dataset, args.batch_size, repeat=False, shuffle=False)
+    index_list = {idx: 0 for idx in test_index}
+    test_iterator = chainer.iterators.MultiprocessIterator(
+        test_dataset, args.batch_size, repeat=False, shuffle=False)
 
     with chainer.no_backprop_mode():
         for i, batch in tqdm.tqdm(enumerate(test_iterator), total=len(test_dataset) // args.batch_size):
@@ -110,23 +118,25 @@ def main():
                 network_manager(mode='test', **stage_input)
                 variables['__stage__'] = j
                 variables.update(network_manager.variables)
-            
+
             # Save images
             try:
-                save_images(args.output_dir, variables, save_image_list, index_list)
+                save_images(args.output_dir, variables,
+                            save_image_list, index_list)
             except KeyError:
-                for name in save_image_list:
-                    print('\n'.join([ str(node) for node in network_manager.validate_network(name) ]))
+                print('\n'.join([str(node)
+                                 for node in network_manager.validate_network()]))
                 raise
+
 
 def save_images(output_dir, variables, save_image_list, index_list):
     for key, output_filename in save_image_list.items():
         image_name = key[0]
         image = deepnet.utils.unwrapped(variables[image_name])
         spacing = variables['spacing'] if len(key) == 1 else variables[key[1]]
-        
+
         if isinstance(image, list):
-            image = np.asarray(image) 
+            image = np.asarray(image)
 
         for i in range(image.shape[0]):
             case_name = variables['case_name'][i]
@@ -135,11 +145,13 @@ def save_images(output_dir, variables, save_image_list, index_list):
             # make output dir
             current_output_dir = os.path.join(output_dir, case_name)
             os.makedirs(current_output_dir, exist_ok=True)
-            
+
             # save images
-            current_output_filename = os.path.join(current_output_dir, output_filename.format(**variables))
+            current_output_filename = os.path.join(
+                current_output_dir, output_filename.format(**variables))
             save_image(current_output_filename, image[i], spacing[i])
-        
+
+
 def save_image(output_filename, image, spacing):
     if image.shape[0] == 1:
         image = image[0]
@@ -148,26 +160,34 @@ def save_image(output_filename, image, spacing):
         spacing = tuple(spacing) + (1,) * (image.ndim - len(spacing))
     deepnet.utils.visualizer.save_image(output_filename, image, spacing)
 
+
 def build_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--gpu', type=int, nargs="*", default=[0], help='gpu id')
-    parser.add_argument('--batch-size', type=int, nargs="*", default=[5], help='batch size')
+    parser.add_argument('--gpu', type=int, nargs="*",
+                        default=[0], help='gpu id')
+    parser.add_argument('--batch-size', type=int, nargs="*",
+                        default=[5], help='batch size')
 
-    parser.add_argument('--dataset-config', type=str, required=True, help='A dataset configuraiton written by extended toml format.')
+    parser.add_argument('--dataset-config', type=str, required=True,
+                        help='A dataset configuraiton written by extended toml format.')
     parser.add_argument('--network-config', type=str, default=None)
-    
+
     parser.add_argument('--test-index', type=str, help='training indices text')
-    parser.add_argument('--n-max-test-iter', type=int, default=60000, help='Max iteration of train.')
+    parser.add_argument('--n-max-test-iter', type=int,
+                        default=60000, help='Max iteration of train.')
 
     parser.add_argument('--log-root-dir', type=str, default='./log/')
-    parser.add_argument('--log-index', type=int, default=None, help='Log direcotry index for training.')
+    parser.add_argument('--log-index', type=int, default=None,
+                        help='Log direcotry index for training.')
     parser.add_argument('--step-index', type=int, default=1, help='step index')
 
-    parser.add_argument('--save', type=str, required=True, nargs='+', help='Paired string of variable name and save format. Save format can be used variable such as __index__.')
+    parser.add_argument('--save', type=str, required=True, nargs='+',
+                        help='Paired string of variable name and save format. Save format can be used variable such as __index__.')
     parser.add_argument('--output-dir', type=str, default=None)
     parser.add_argument('--mode', type=str, default='none')
 
     return parser
+
 
 def str2bool(string):
     string = string.lower()
@@ -177,13 +197,15 @@ def str2bool(string):
         return False
     else:
         raise ValueError('Unknown flag value: {}'.format(string))
-    
+
+
 def update_log_dir(network_config, log_dir):
     network_config['log_dir'] = log_dir['root']
     network_config['visualize_dir'] = log_dir['visualize']
     network_config['archive_dir'] = log_dir['archive']
     network_config['param_dir'] = log_dir['param']
     return network_config
+
 
 if __name__ == '__main__':
     main()
