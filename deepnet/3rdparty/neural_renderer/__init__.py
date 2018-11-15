9dd5a8cf3d6c95142654f9459036bfea92e2eeeb
elda27 <kaz.birdstick@gmail.com>
2018-11-15 16:11:19 +0900

    Changes for import error

--- deepnet/3rdparty/neural_renderer/__init__.py
+++ deepnet/3rdparty/neural_renderer/__init__.py
@@ -1,16 +1,16 @@
-from cross import cross
-from get_points_from_angles import get_points_from_angles
-from lighting import lighting
-from load_obj import load_obj
-from look import look
-from look_at import look_at
-from mesh import Mesh
-from optimizers import Adam
-from perspective import perspective
-from rasterize import (
+from neural_renderer.cross import cross
+from neural_renderer.get_points_from_angles import get_points_from_angles
+from neural_renderer.lighting import lighting
+from neural_renderer.load_obj import load_obj
+from neural_renderer.look import look
+from neural_renderer.look_at import look_at
+from neural_renderer.mesh import Mesh
+from neural_renderer.optimizers import Adam
+from neural_renderer.perspective import perspective
+from neural_renderer.rasterize import (
     rasterize_rgbad, rasterize, rasterize_silhouettes, rasterize_depth, use_unsafe_rasterizer, Rasterize)
-from renderer import Renderer
-from save_obj import save_obj
-from vertices_to_faces import vertices_to_faces
+from neural_renderer.renderer import Renderer
+from neural_renderer.save_obj import save_obj
+from neural_renderer.vertices_to_faces import vertices_to_faces
 
 __version__ = '1.1.3'
