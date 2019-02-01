9daf8b7b04c266a1b0601a9383cd684adb54ad3e
elda27 <kaz.birdstick@gmail.com>
2019-02-01 13:31:11 +0900

    Modify a resize to keep aspect

--- deepnet/process/data_augmentation/image_augmentation.py
+++ deepnet/process/data_augmentation/image_augmentation.py
@@ -1,6 +1,8 @@
 import numpy as np
-import chainer.cuda
+import chainer
+from chainer.backends import cuda
 from . import utils
+
 
 def random_rotation(x, rg, row_axis=1, col_axis=2, channel_axis=0,
                     fill_mode='nearest', cval=0.):
@@ -19,7 +21,7 @@ def random_rotation(x, rg, row_axis=1, col_axis=2, channel_axis=0,
     # Returns
         Rotated Numpy image tensor.
     """
-    xp = chainer.cuda.get_array_module(x)
+    xp = cuda.get_array_module(x)
     theta = xp.pi / 180 * xp.random.uniform(-rg, rg)
     rotation_matrix = xp.array([[xp.cos(theta), -xp.sin(theta), 0],
                                 [xp.sin(theta), xp.cos(theta), 0],
@@ -49,7 +51,7 @@ def random_shift(x, wrg, hrg, row_axis=1, col_axis=2, channel_axis=0,
     # Returns
         Shifted Numpy image tensor.
     """
-    xp = chainer.cuda.get_array_module(x)
+    xp = cuda.get_array_module(x)
     h, w = x.shape[row_axis], x.shape[col_axis]
     tx = xp.random.uniform(-hrg, hrg) * h
     ty = xp.random.uniform(-wrg, wrg) * w
@@ -79,7 +81,7 @@ def random_shear(x, intensity, row_axis=1, col_axis=2, channel_axis=0,
     # Returns
         Sheared Numpy image tensor.
     """
-    xp = chainer.cuda.get_array_module(x)
+    xp = cuda.get_array_module(x)
     shear = xp.random.uniform(-intensity, intensity)
     shear_matrix = xp.array([[1, -xp.sin(shear), 0],
                              [0, xp.cos(shear), 0],
@@ -110,7 +112,7 @@ def random_zoom(x, zoom_range, row_axis=1, col_axis=2, channel_axis=0,
     # Raises
         ValueError: if `zoom_range` isn't a tuple.
     """
-    xp = chainer.cuda.get_array_module(x)
+    xp = cuda.get_array_module(x)
     if len(zoom_range) != 2:
         raise ValueError('`zoom_range` should be a tuple or list of two floats. '
                          'Received arg: ', zoom_range)
@@ -128,8 +130,9 @@ def random_zoom(x, zoom_range, row_axis=1, col_axis=2, channel_axis=0,
     x = apply_transform(x, transform_matrix, channel_axis, fill_mode, cval)
     return x
 
+
 def transform_matrix_offset_center(matrix, x, y):
-    xp = chainer.cuda.get_array_module(x)
+    xp = cuda.get_array_module(x)
 
     o_x = float(x) / 2 + 0.5
     o_y = float(y) / 2 + 0.5
@@ -158,7 +161,7 @@ def apply_transform(x,
     # Returns
         The transformed version of the input.
     """
-    xp = chainer.cuda.get_array_module(x)
+    xp = cuda.get_array_module(x)
     x = xp.rollaxis(x, channel_axis, 0)
     final_affine_matrix = transform_matrix[:2, :2]
     final_offset = transform_matrix[:2, 2]
@@ -175,12 +178,13 @@ def apply_transform(x,
 
 
 def flip_axis(x, axis):
-    xp = chainer.cuda.get_array_module(x)
+    xp = cuda.get_array_module(x)
     x = xp.asarray(x).swapaxes(axis, 0)
     x = x[::-1, ...]
     x = x.swapaxes(0, axis)
     return x
-    
+
+
 class ImageDataGenerator(object):
     """Generate image data with data augmentation.
     Arguments:
@@ -196,7 +200,6 @@ class ImageDataGenerator(object):
         vertical_flip: whether to randomly flip images vertically.
     """
 
-
     def __init__(self,
                  rotation_range=0.0,
                  width_shift_range=0.0,
@@ -207,6 +210,7 @@ class ImageDataGenerator(object):
                  cval=0.0,
                  horizontal_flip=False,
                  vertical_flip=False,
+                 keep_aspect=True
                  ):
 
         self.rotation_range = rotation_range
@@ -216,16 +220,17 @@ class ImageDataGenerator(object):
         self.zoom_range = zoom_range
         self.intensity_range = intensity_range
         self.cval = cval
-        
+
         self.horizontal_flip = horizontal_flip
         self.vertical_flip = vertical_flip
 
         self.row_axis = 1
         self.col_axis = 2
         self.channel_axis = 3
-        
-        self.interp_order=1
+
+        self.interp_order = 1
         self.fill_mode = 'nearest'
+        self.keep_aspect = keep_aspect
 
         if np.isscalar(zoom_range):
             self.zoom_range = [1 - zoom_range, 1 + zoom_range]
@@ -236,18 +241,16 @@ class ImageDataGenerator(object):
                              'a tuple or list of two floats. '
                              'Received arg: ', zoom_range)
 
-
-
     def random_transform(self, x):
-        xp = chainer.cuda.get_array_module(x)
+        xp = cuda.get_array_module(x)
 
         # x is a single image, so it doesn't have image number at index 0
         img_row_axis = self.row_axis - 1
         img_col_axis = self.col_axis - 1
         img_channel_axis = self.channel_axis - 1
 
-        if seed is not None:
-            xp.random.seed(seed)
+        # if seed is not None:
+        #     xp.random.seed(seed)
 
         input_shape = x.shape
 
@@ -255,17 +258,20 @@ class ImageDataGenerator(object):
         # use composition of homographies
         # to generate final transform that needs to be applied
         if self.rotation_range:
-            self.theta = xp.pi / 180 * xp.random.uniform(-self.rotation_range, self.rotation_range)
+            self.theta = xp.pi / 180 * \
+                xp.random.uniform(-self.rotation_range, self.rotation_range)
         else:
             self.theta = 0
 
         if self.height_shift_range:
-            self.tx = xp.random.uniform(-self.height_shift_range, self.height_shift_range) * input_shape[img_row_axis]
+            self.tx = xp.random.uniform(-self.height_shift_range,
+                                        self.height_shift_range) * input_shape[img_row_axis]
         else:
             self.tx = 0
 
         if self.width_shift_range:
-            self.ty = xp.random.uniform(-self.width_shift_range, self.width_shift_range) * input_shape[img_col_axis]
+            self.ty = xp.random.uniform(-self.width_shift_range,
+                                        self.width_shift_range) * input_shape[img_col_axis]
         else:
             self.ty = 0
 
@@ -277,22 +283,26 @@ class ImageDataGenerator(object):
         if self.zoom_range[0] == 1 and self.zoom_range[1] == 1:
             self.zx, self.zy = 1, 1
         else:
-            self.zx, self.zy = xp.random.uniform(self.zoom_range[0], self.zoom_range[1], 2)
+            self.zx, self.zy = xp.random.uniform(
+                self.zoom_range[0], self.zoom_range[1], 2)
+        if self.keep_aspect:
+            self.zy = self.zx
 
         if self.horizontal_flip:
             self.horizontal_flip_prob = xp.random.random()
         else:
             self.horizontal_flip_prob = 0
-            
+
         if self.vertical_flip:
-             self.vertical_flip_prob = xp.random.random()
+            self.vertical_flip_prob = xp.random.random()
         else:
-            self.vertical_flip_prob = 0                   
+            self.vertical_flip_prob = 0
 
         self.transform_matrix = None
         if self.theta != 0:
             rotation_matrix = xp.array([[xp.cos(self.theta), -xp.sin(self.theta), 0],
-                                        [xp.sin(self.theta), xp.cos(self.theta), 0],
+                                        [xp.sin(self.theta), xp.cos(
+                                            self.theta), 0],
                                         [0, 0, 1]], dtype=xp.float32)
             self.transform_matrix = rotation_matrix
 
@@ -300,61 +310,63 @@ class ImageDataGenerator(object):
             shift_matrix = xp.array([[1, 0, self.tx],
                                      [0, 1, self.ty],
                                      [0, 0, 1]], dtype=xp.float32)
-            self.transform_matrix = shift_matrix if self.transform_matrix is None else xp.dot(self.transform_matrix, shift_matrix)
+            self.transform_matrix = shift_matrix if self.transform_matrix is None else xp.dot(
+                self.transform_matrix, shift_matrix)
 
         if self.shear != 0:
             shear_matrix = xp.array([[1, -xp.sin(self.shear), 0],
-                                    [0, xp.cos(self.shear), 0],
-                                    [0, 0, 1]], dtype=xp.float32)
-            self.transform_matrix = shear_matrix if self.transform_matrix is None else xp.dot(self.transform_matrix, shear_matrix)
+                                     [0, xp.cos(self.shear), 0],
+                                     [0, 0, 1]], dtype=xp.float32)
+            self.transform_matrix = shear_matrix if self.transform_matrix is None else xp.dot(
+                self.transform_matrix, shear_matrix)
 
         if self.zx != 1 or self.zy != 1:
             zoom_matrix = xp.array([[self.zx, 0, 0],
                                     [0, self.zy, 0],
                                     [0, 0, 1]], dtype=xp.float32)
-            self.transform_matrix = zoom_matrix if self.transform_matrix is None else xp.dot(self.transform_matrix, zoom_matrix)
-
+            self.transform_matrix = zoom_matrix if self.transform_matrix is None else xp.dot(
+                self.transform_matrix, zoom_matrix)
 
         if self.transform_matrix is not None:
             h, w = input_shape[img_row_axis], input_shape[img_col_axis]
-            self.transform_matrix = transform_matrix_offset_center(self.transform_matrix, h, w)
-
+            self.transform_matrix = transform_matrix_offset_center(
+                self.transform_matrix, h, w)
 
         if self.intensity_range != 0:
-            self.intensity = xp.random.uniform(-self.intensity_range, self.intensity_range)
+            self.intensity = xp.random.uniform(
+                -self.intensity_range, self.intensity_range)
         else:
             self.intensity = 0.
-            
 
-        # """ apply transform """        
+        # """ apply transform """
         return self.fixed_transform(x)
-        
+
     def fixed_transform(self, input):
         x = None
+        xp = cuda.get_array_module(input)
         if isinstance(x, chainer.Variable):
             x = xp.copy(input.data.astype(xp.float32))
         else:
             x = xp.copy(input.astype(xp.float32))
 
-        xp = chainer.cuda.get_array_module(x)
+        xp = cuda.get_array_module(x)
 
         # x is a single image, so it doesn't have image number at index 0
         img_row_axis = self.row_axis - 1
         img_col_axis = self.col_axis - 1
         img_channel_axis = self.channel_axis - 1
 
-        if self.transform_matrix is not None:            
+        if self.transform_matrix is not None:
             x = apply_transform(x, self.transform_matrix, img_channel_axis,
-                                fill_mode=self.fill_mode, 
-                                cval=self.cval, 
+                                fill_mode=self.fill_mode,
+                                cval=self.cval,
                                 interp_order=self.interp_order)
-                                
+
         if self.horizontal_flip_prob > 0.5:
             x = flip_axis(x, img_col_axis)
-            
+
         if self.vertical_flip_prob > 0.5:
             x = flip_axis(x, img_row_axis)
-            
 
         x = x + self.intensity
 
